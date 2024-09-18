a5f7015fa63cf878a0ae1649d7c53e7c1252e414
David Oduor <davidootinez@gmail.com>
2024-09-18 20:35:51 +0300

    modified:   src/app/(dashboard)/layout.tsx 	modified:   src/components/Menu.tsx 	modified:   src/components/Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center justify-between p-4">
+      {/* SEARCH BAR */}
+      <div className="hidden md:flex">
+        <Image src="/search.png" alt="" width={14} height={14} />
+        <input type="text" placeholder="Search..." />
+
+        {/* ICONS AND USER */}
+        <div className="flex items-center justify-between p-4">
+          <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+            <Image src="/message.png" alt="" width={20} height={20} />
+          </div>
+          <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+            <Image src="/announcement.png" alt="" width={20} height={20} />
+          </div>
+          <div className="flex flex-col">
+            <span className="text-xs leading-3 font-medium">John Doe</span>
+            <span className="text-[10px] text-gray-500 text-right">Admin</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
