4b7ae6e426d249b05866f85c18afb7c601caae1d
David Oduor <davidootinez@gmail.com>
2024-10-19 19:20:11 +0300

    modified:   src/components/FormModal.tsx 	new file:   src/components/forms/TeacherForm.tsx

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import React, { useState } from "react";
+import TeacherForm from "./forms/TeacherForm";
 
 export default function FormModal({
   table,
@@ -45,7 +46,7 @@ export default function FormModal({
         <button className="bg-red-700  text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
       </form>
     ) : (
-      "create or update form"
+      <TeacherForm type="create" />
     );
   };
   return (
